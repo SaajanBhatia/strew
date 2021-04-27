78c532bc674a402bdbe94ce62df46bf6da118f77
SaajanBhatia <Saajan@ssbhatia.co.uk>
2021-04-27 19:04:00 +0100

    Python Updates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+files = {
+    'Home' : 'index.html'
+}
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    return 'welcome to strew'
+    return render_template(files['Home'])
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(port=8141)
---
+++ forms.py
@@ -0,0 +1,11 @@
+
+
+class Forms(object):
+    def __init__(self):
+        pass
+
+    def __UserSignUp(data):
+        pass
+
+    def __UserLogIn(data):
+        pass
