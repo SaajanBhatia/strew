859ddfd24fc8239fe4592df0dbd798e469417e0d
SaajanBhatia <Saajan@ssbhatia.co.uk>
2021-04-27 17:32:56 +0100

    Python Update

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print ('hello strew')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    return 'welcome to strew'
+
+if __name__ == "__main__":
+    app.run(port=8141)
